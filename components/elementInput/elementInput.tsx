51b47610d3870b67042f5570607ff318524a0553
MgShepherd <michaelshep1504@gmail.com>
2024-01-25 20:15:52 +0000

    Added some styling to the ElementInput component

--- components/elementInput/elementInput.tsx
+++ components/elementInput/elementInput.tsx
@@ -16,7 +16,7 @@ const ElementInput = ({ setElements }: ElementProps) => {
       let result = parseInt(value);
       if (isNaN(result)) {
         setErrorMessage(
-          'Please ensure you enter a list of comma seperate integers',
+          'Please ensure you enter a list of comma seperated integers',
         );
         return;
       }
@@ -26,20 +26,27 @@ const ElementInput = ({ setElements }: ElementProps) => {
     setElements((prevElements) => prevElements.concat(valuesToAdd));
   };
 
-  const handleClear = () => {
-    setElements([]);
-  };
+  const buttonStyle = 'bg-blue-400 p-1 border-white border-2 rounded w-full';
 
   return (
-    <div>
-      <p>Enter Numbers to sort (comma seperated)</p>
-      <input
-        type="text"
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Submit Values</button>
-      <button onClick={handleClear}>Clear Existing Values</button>
+    <div className="flex w-full items-center flex-col">
+      <div>
+        <p>Enter Numbers to sort (comma seperated)</p>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          className="bg-stone-600 border-white border-2 rounded px-2 py-1 w-full"
+        />
+        <div className="mt-2 flex justify-between gap-2">
+          <button onClick={handleSubmit} className={buttonStyle}>
+            Submit
+          </button>
+          <button onClick={() => setElements([])} className={buttonStyle}>
+            Clear Existing
+          </button>
+        </div>
+      </div>
       {errorMessage != '' && <p>{errorMessage}</p>}
     </div>
   );
